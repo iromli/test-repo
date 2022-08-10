e6c52201577d03091eae79f74840eb71d3554d6c
iromli <isman.firmansyah@gmail.com>
2022-08-10 21:51:17 +0700

    test: add more violations

--- demo.py
+++ demo.py
@@ -2,5 +2,12 @@ import subprocess
 
 password = ""
 
+table = "orgs"
+org_name = "demo"
+query = f"SELECT FROM {table} where name = '{org_name}'"
+
+if not "foo" in "foo bar baz lorem ipsum":
+    ...
+
 
 help(subprocess)
