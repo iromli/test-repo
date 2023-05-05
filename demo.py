64e92a72bc4bcb88f0d663861733131696559ac3
Isman Firmansyah <iromli@users.noreply.github.com>
2023-05-06 01:30:54 +0700

    test: add vuln (#20)
    * test: add vuln
    
    * test: add more violations

--- demo.py
+++ demo.py
@@ -1,5 +1,14 @@
 import subprocess
 
+password = ""
+
+table = "orgs"
+org_name = "demo"
+query = f"SELECT FROM {table} where name = '{org_name}'"
+
+if not "foo" in "foo bar baz lorem ipsum":
+    ...
+
 
 help(subprocess)
 
